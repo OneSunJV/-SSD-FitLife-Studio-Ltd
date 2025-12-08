7fe51dcaebe3fb1ccacf7c8077ea660b391d17b2
Thomas Creasey <56192561+ThomasCreasey@users.noreply.github.com>
2025-12-08 21:29:44 +0000

    Fix errors

--- controllers/system_layout.py
+++ controllers/system_layout.py
@@ -8,7 +8,12 @@ class SystemLayoutController:
         systemLayout = system_layout.SystemLayout(root)
         pageContainer = systemLayout.get_page_container()
         pageController = PageController(pageContainer)
-        system_layout.SystemLayout.create_sidebar_buttons(PAGES, pageController)
+        systemLayout.create_sidebar_buttons(PAGES, pageController)
+
         for page in PAGES:
             pageController.register_page(page["className"])
-        pageController.display_page(next(filter(PAGES, lambda page: page["default"] == True))["className"])
+
+        for idx, page in enumerate(PAGES):
+            if page["default"] == True:
+                pageController.display_page(idx)
+                break
