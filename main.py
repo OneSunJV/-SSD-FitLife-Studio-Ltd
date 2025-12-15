738789ee5b95668c755ac4d087c2e65a2455b8a9
Thomas Creasey <56192561+ThomasCreasey@users.noreply.github.com>
2025-12-15 15:54:40 +0000

    Merge main and simplify

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-from controllers.login_layout import LoginLayoutController
 import tkinter as tk
 
-global window
+from layouts.login_layout import LoginLayout
 
-defaultSystemPage = 0
-
-class Window():
-    def __init__(self):          
-        self.root = tk.Tk()
-        self.root.title('Login Page')
-        self.root.geometry('1280x720')
-        self.root.resizable(False, False)
-        self.defaultSystemPage = 0
-        LoginLayoutController.load_login(self.root)
-
-    def run_mainloop(self):
-        self.root.mainloop()
-
-        
-root = Window()
-root.run_mainloop()
+LoginLayout()
