117855c22d65b639ad80cf24c9cd8dd294643932
Jonathan <trivettjonathan@gmail.com>
2026-01-05 14:21:06 +0000

    Updated to main branch 2x

--- config/pages.py
+++ config/pages.py
@@ -1,9 +1,9 @@
 from pages import calendar
-from pages import calendar2
+from pages import dashboard
 
 global PAGES
 
 PAGES = [
     { "displayName": "Calendar", "className": calendar.CalendarPage, "default": True },
-    { "displayName": "Calendar2", "className": calendar2.Calendar2Page, "default": False }
+    { "displayName": "Dashboard", "className": dashboard.FitLifeDashboard, "default": False }
 ]
