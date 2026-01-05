a842da92a28a4afddf5f6590c53c9cb02f9792f0
OneSunJV <jaisonvarghese1000@gmail.com>
2026-01-05 11:24:36 +0000

    Created database

--- database/manager.py
+++
@@ -1,9 +0,0 @@
-from peewee import *
-
-class DatabaseManager:
-    def __init__(self):
-        self.db = SqliteDatabase('database.db')
-
-    def create_tables(self):
-        self.db.connect()
-        #self.db.create_tables()
