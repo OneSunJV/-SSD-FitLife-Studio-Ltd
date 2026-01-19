ec0141cb347ac0955390acc1fd39f1aa656b8e28
Thomas Creasey <56192561+ThomasCreasey@users.noreply.github.com>
2026-01-19 16:39:11 +0000

    Documentation

--- session.py
+++ session.py
@@ -5,4 +5,4 @@ class Session:
     def set(self, user_id):
         self.user_id = user_id
 
-session = Session()
+session = Session() # Session is a singleton; there can only be one session per instance of the application.
