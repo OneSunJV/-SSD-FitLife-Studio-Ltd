734eafead295c9521989231f4bd1e1239e1af8e6
Thomas Creasey <56192561+ThomasCreasey@users.noreply.github.com>
2026-01-19 12:31:48 +0000

    Randomly seed database with sessions

--- database/seed_db.py
+++ database/seed_db.py
@@ -1,4 +1,7 @@
+import random
 import sqlite3
+from datetime import datetime, timedelta
+from calendar import monthrange
 
 connection = sqlite3.connect('SystemDatabase.db')
 cursor_object = connection.cursor()
@@ -13,10 +16,48 @@ connection.execute('''INSERT INTO Classes
                    VALUES 
                    (1, 'Strength Training', 'Strength Training Description');''')
 
-connection.execute('''INSERT INTO Sessions 
-                        (SessionID, ClassID, TrainerID, SessionStartTime, SessionFinishTime, SessionDate)  
-                      VALUES 
-                        (1, 1, 1, '1769337000', '1769340600', '2026-01-25');''') # 2026-01-25 10:30-11:30
+connection.execute('''INSERT INTO Classes 
+                   (ClassID, ClassType, ClassDescription)  
+                   VALUES 
+                   (2, 'HIIT', 'HIIT Description');''')
+
+connection.execute('''INSERT INTO Classes 
+                   (ClassID, ClassType, ClassDescription)  
+                   VALUES 
+                   (3, 'Spin Class', 'Spin Class Description');''')
+
+connection.execute('''INSERT INTO Classes 
+                   (ClassID, ClassType, ClassDescription)  
+                   VALUES 
+                   (4, 'Dance Class', 'Dance Class Description');''')
+
+connection.execute('''INSERT INTO Classes 
+                   (ClassID, ClassType, ClassDescription)  
+                   VALUES 
+                   (5, 'Yoga', 'Yoga Description');''')
+
+
+current_month = datetime.now().month
+current_year = datetime.now().year
+
+_, days_in_month = monthrange(current_year, current_month)
+
+idx = 1
+for day in range(1, days_in_month + 1):
+    date_str = f'{current_year}-{current_month:02}-{day:02}'
+
+    for i in range(0, random.randint(0,5)):
+        class_id = random.randint(1, 5)
+
+        start_hour = random.randint(0,23)
+        start_time = datetime(current_year, current_month, day, start_hour).timestamp()
+        finish_time = start_time + 1 * 60 * 60
+
+        connection.execute(f'''INSERT INTO Sessions
+                              (SessionID, ClassID, TrainerID, SessionStartTime, SessionFinishTime, SessionDate)
+                              VALUES ({idx}, {class_id}, 1, '{start_time}', '{finish_time}', '{date_str}');''')
+
+        idx += 1
 
 connection.commit()
 connection.close()
