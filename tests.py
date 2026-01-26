e63186e26d7a080dea2f8a4630d845558fd43141
Thomas Creasey <56192561+ThomasCreasey@users.noreply.github.com>
2026-01-26 13:41:14 +0000

    Update unit tests

--- tests.py
+++ tests.py
@@ -11,7 +11,7 @@ class TestLoginPage(unittest.TestCase):
     def setUp(self):
         create_database()
 
-    def test_login_credential_validation(self):
+    def test_credential_validation_with_valid_credentials(self):
         # SETUP
         with sqlite3.connect('SystemDatabase.db') as connection:
             password = "TestPassword"
@@ -25,10 +25,29 @@ class TestLoginPage(unittest.TestCase):
             connection.commit()
 
         # EXECUTE
-        result = validate_login_credentials("TestUser", "TestPassword")
+        (valid, _row) = validate_login_credentials("TestUser", "TestPassword")
 
         # VERIFY
-        self.assertTrue(result, "Expected login credentials to be valid")
+        self.assertTrue(valid, "Expected login credentials to be valid")
+
+    def test_credential_validation_with_invalid_credentials(self):
+        # SETUP
+        with sqlite3.connect('SystemDatabase.db') as connection:
+            password = "TestPassword"
+            password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            connection.execute('''INSERT INTO Employees
+                                  (EmployeeID, Username, PasswordHash, FirstName, LastName, DateOfBirth, EmailAddress,
+                                   PhoneNumber, JoinDate, EmployeeType)
+                                  VALUES (9998, 'TestUser', ?, 'Test', 'User', '2000-01-01', 'test@gmail.com',
+                                          '07000000000',
+                                          '2026-01-26', 'ADMIN');''', (password_hash,))
+            connection.commit()
+
+        # EXECUTE
+        (valid, _row) = validate_login_credentials("TestUser", "SomeInvalidPassword") # Note that in this case the correct password should be TestPassword
+
+        # VERIFY
+        self.assertFalse(valid, "Expected login credentials to be invalid")
 
     def tearDown(self):
         teardown()
